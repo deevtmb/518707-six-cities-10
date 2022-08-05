6d34cf91e970c54d5da9a08ff18ca655efd081cc
Andrei <l2.ls@yandex.ru>
2022-08-05 18:43:23 +0800

    Настройка авторизации

--- project/src/pages/login-screen/login-screen.tsx
+++ project/src/pages/login-screen/login-screen.tsx
@@ -1,13 +1,42 @@
+import { FormEvent, useLayoutEffect, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../../components/logo/logo';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { loginAction } from '../../store/api-actions';
+
 export default function LoginScreen(): JSX.Element {
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (emailRef.current !== null && passwordRef.current !== null) {
+      dispatch(loginAction({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      }));
+    }
+  };
+
+  useLayoutEffect(() => {
+    if (authorizationStatus === AuthorizationStatus.Authorized) {
+      navigate(AppRoute.Main);
+    }
+  }, [authorizationStatus, navigate]);
+
   return (
     <div className="page page--gray page--login">
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              <Logo />
             </div>
           </div>
         </div>
@@ -17,23 +46,37 @@ export default function LoginScreen(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form className="login__form form" action="#" method="post" onSubmit={handleFormSubmit}>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
+                <input
+                  className="login__input form__input"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                  ref={emailRef}
+                />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+                <input
+                  className="login__input form__input"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                  ref={passwordRef}
+                />
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
           </section>
           <section className="locations locations--login locations--current">
             <div className="locations__item">
-              <a className="locations__item-link" href="#">
+              <Link className="locations__item-link" to="/">
                 <span>Amsterdam</span>
-              </a>
+              </Link>
             </div>
           </section>
         </div>
--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,8 +1,10 @@
 import {createAction} from '@reduxjs/toolkit';
 import {Offer} from '../types/offer';
 
-export const changeSelectedCity = createAction<{city: string}>('changeSelectedCity');
+export const changeSelectedCity = createAction<string>('changeSelectedCity');
 
 export const loadOffers = createAction<Offer[]>('data/loadOffers');
 
 export const setDataLoadingStatus = createAction<boolean>('data/setDataLoadingStatus');
+
+export const setAuthorizationStatus = createAction<string>('user/setAuthorizationStatus');
