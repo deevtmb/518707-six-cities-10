f559e80c4d734f85f814a7d3a17f3cd3bb8e2135
Andrei <l2.ls@yandex.ru>
2022-08-11 22:39:11 +0800

    Добавлено отображение ошибок с react-toastify

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/app/app';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {store} from './store';
 import { fetchOffersAction, checkAuthorizationAction } from './store/api-actions';
 
@@ -17,6 +19,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <ToastContainer />
         <App />
       </BrowserRouter>
     </Provider>
